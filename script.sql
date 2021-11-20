45c204236f00974726f0170a89ec89448f9b63b2
Praveen VS <69992086+vsp2798@users.noreply.github.com>
2021-11-19 20:35:58 -0500

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,10 +1,13 @@
 /*
 
-Covid 19 Data Exploration
+Covid 19 Data Exploration using PostgreSQL
 
-Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+Skills used: Joins, CTE's, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
 
 */
+--------------------------------------------------------------------------------------------------------------------------------------------------
+---- FEW MANDATORY EXPLORATION
+
 SELECT *
 FROM VACCINATIONS;
 
@@ -12,7 +15,7 @@ FROM VACCINATIONS;
 SELECT *
 FROM COVIDDEATHS
 WHERE CONTINENT IS NOT NULL
-ORDER BY 3,4 -------------------------------------------------------------------------
+ORDER BY 3,4 
 
 SELECT LOCATION1,
 	DATE1,
@@ -22,9 +25,13 @@ SELECT LOCATION1,
 	POPULATION
 FROM COVIDDEATHS
 WHERE CONTINENT IS NOT NULL
-ORDER BY 1,2 -------------------------------------------------------------------------
- -- Total Cases vs Total Deaths
+ORDER BY 1,2 
+
+--------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Total Cases vs Total Deaths
 -- Shows likelihood of dying if you contract covid in your country
+-- This query says that at the end of October that If you contract covid and live in Canada there is a 1.68 % chance of dying.
 
 SELECT LOCATION1,
 	DATE1,
@@ -34,9 +41,11 @@ SELECT LOCATION1,
 FROM COVIDDEATHS
 WHERE LOCATION1 = 'Canada'
 ORDER BY 1,
-	2 DESC -- This query says that at the end of October that If you contract covid and live in Canada there is a 1.68 % chance of dying.
- -------------------------------------------------------------------------
- -- This query tells the highest infection count and highest population infected in a country and its says that Montenegro has the highest infected popultation and canada ranks 94TH
+	2 DESC 
+
+--------------------------------------------------------------------------------------------------------------------------------------------------
+
+--This query tells the highest infection count and highest population infected in a country and its says that Montenegro has the highest infected popultation and canada ranks 94TH
 
 SELECT LOCATION1,
 	POPULATION,
@@ -48,18 +57,23 @@ WHERE TOTAL_CASES IS NOT NULL
 	AND CONTINENT IS NOT NULL
 GROUP BY LOCATION1,
 	POPULATION
-ORDER BY PERCENTPOPULATIONINFECTED DESC -------------------------------------------------------------------------
- -- Countries with Highest Death Count per Population and it shows that Unites States has highest number of death counts among all countries
+ORDER BY PERCENTPOPULATIONINFECTED DESC 
+
+--------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Countries with Highest Death Count per Population and it shows that Unites States has highest number of death counts among all countries
 
 SELECT LOCATION1,
 	MAX(TOTAL_DEATHS) AS TOTALDEATHCOUNT
 FROM COVIDDEATHS
-WHERE TOTAL_DEATHS IS NOT NULL
-	AND CONTINENT IS NOT NULL
+WHERE TOTAL_DEATHS IS NOT NULL AND CONTINENT IS NOT NULL
 GROUP BY LOCATION1
-ORDER BY TOTALDEATHCOUNT DESC -------------------------------------------------------------------------
- -- GLOBAL NUMBERS AND PERCENTAGES
- --- Death percentage till now
+ORDER BY TOTALDEATHCOUNT DESC 
+
+--------------------------------------------------------------------------------------------------------------------------------------------------
+ 
+-- GLOBAL NUMBERS AND PERCENTAGES
+--- Death percentage till now
 
 SELECT SUM(NEW_CASES) AS TOTAL_CASES,
 	SUM(NEW_DEATHS) AS TOTAL_DEATHS,
@@ -67,61 +81,66 @@ SELECT SUM(NEW_CASES) AS TOTAL_CASES,
 FROM COVIDDEATHS --Where location like '%states%'
 
 WHERE NEW_CASES IS NOT NULL
-	AND CONTINENT IS NOT NULL --Group By date1
-ORDER BY 1,2 -------------------------------------------------------------------------
- --- This query shows the number of vaccinations increasing each day in a country
+AND CONTINENT IS NOT NULL 
+--Group By date1
+ORDER BY 1,2 
+
+--------------------------------------------------------------------------------------------------------------------------------------------------
+ 
+--- This query shows the number of vaccinations increasing each day in a country
 
 SELECT CD.CONTINENT,
 	CD.LOCATION1,
 	CD.DATE1,
 	CD.POPULATION,
 	VC.NEW_VACCINATIONS,
-	SUM(VC.NEW_VACCINATIONS) OVER (PARTITION BY CD.LOCATION1
-																																ORDER BY CD.LOCATION1,
-																																	CD.DATE1) AS ROLLINGPEOPLEVACCINATED
+	SUM(VC.NEW_VACCINATIONS) OVER (PARTITION BY CD.LOCATION1 ORDER BY CD.LOCATION1,CD.DATE1) AS ROLLINGPEOPLEVACCINATED
+
 FROM COVIDDEATHS CD
 JOIN VACCINATIONS VC ON CD.LOCATION1 = VC.LOCATION2
 AND CD.DATE1 = VC.DATE2
 WHERE CD.CONTINENT IS NOT NULL
-ORDER BY 2,3 -------------------------------------------------------------------------
- -- Vaccinations in India till now
- -- CTE is used. Also this query finds out that only 70% of population has been vaccinated as of Nov 1st 2021
- WITH POPULATION_VS_VACCINATIONS (CONTINENT,
+ORDER BY 2,3 
 
-								LOCATION1,
-								DATE1,
-								POPULATION,
-								NEW_VACCINATIONS,
-								ROLLINGPEOPLEVACCINATED) AS
-	(SELECT CD.CONTINENT,
-			CD.LOCATION1,
-			CD.DATE1,
-			CD.POPULATION,
-			VC.NEW_VACCINATIONS,
-			SUM(VC.NEW_VACCINATIONS) OVER (PARTITION BY CD.LOCATION1
-																																		ORDER BY CD.LOCATION1,
-																																			CD.DATE1) AS ROLLINGPEOPLEVACCINATED
-		FROM COVIDDEATHS CD
-		JOIN VACCINATIONS VC ON CD.LOCATION1 = VC.LOCATION2
-		AND CD.DATE1 = VC.DATE2
-		WHERE CD.CONTINENT IS NOT NULL
-		ORDER BY 2,3)
+--------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Vaccinations in India till now
+-- CTE is used. Also this query finds out that only 70% of population in India has been vaccinated as of Nov 1st 2021
+ 
+
+WITH POPULATION_VS_VACCINATIONS (CONTINENT,LOCATION1,DATE1,POPULATION,NEW_VACCINATIONS,ROLLINGPEOPLEVACCINATED) AS
+	
+(
+SELECT CD.CONTINENT,
+       CD.LOCATION1,
+       CD.DATE1,
+       CD.POPULATION,
+       VC.NEW_VACCINATIONS,
+       SUM(VC.NEW_VACCINATIONS) OVER (PARTITION BY CD.LOCATION1 ORDER BY CD.LOCATION1, CD.DATE1) AS ROLLINGPEOPLEVACCINATED
+
+FROM COVIDDEATHS CD
+JOIN VACCINATIONS VC ON CD.LOCATION1 = VC.LOCATION2
+AND CD.DATE1 = VC.DATE2
+WHERE CD.CONTINENT IS NOT NULL
+ORDER BY 2,3)
+
 SELECT *,
 	(ROLLINGPEOPLEVACCINATED / POPULATION) * 100 AS ROLLINGPEOPLEVACCINETEDPERCENTAGE
 FROM POPULATION_VS_VACCINATIONS
-WHERE LOCATION1 = 'India' -------------------------------------------------------------------------
- --- TO CREATE VIEW TO STORE DATA FOR LATER VISUALIZATIONS
+WHERE LOCATION1 = 'India' 
 
-	CREATE VIEW PERCENTPOPULATIONVACCINATED AS
-	SELECT CD.CONTINENT,
-		CD.LOCATION1,
-		CD.DATE1,
-		CD.POPULATION,
-		VC.NEW_VACCINATIONS,
-		SUM(VC.NEW_VACCINATIONS) OVER (PARTITION BY CD.LOCATION1
-																																	ORDER BY CD.LOCATION1,
-																																		CD.DATE1) AS ROLLINGPEOPLEVACCINATED
-	FROM COVIDDEATHS CD
-	JOIN VACCINATIONS VC ON CD.LOCATION1 = VC.LOCATION2
-	AND CD.DATE1 = VC.DATE2 WHERE CD.CONTINENT IS NOT NULL
+--------------------------------------------------------------------------------------------------------------------------------------------------
+
+--- TO CREATE VIEW TO STORE DATA FOR LATER VISUALIZATIONS
+
+CREATE VIEW PERCENTPOPULATIONVACCINATED AS
+SELECT CD.CONTINENT,
+CD.LOCATION1,
+CD.DATE1,
+CD.POPULATION,
+VC.NEW_VACCINATIONS,
+SUM(VC.NEW_VACCINATIONS) OVER (PARTITION BY CD.LOCATION1 ORDER BY CD.LOCATION1, CD.DATE1) AS ROLLINGPEOPLEVACCINATED 
+FROM COVIDDEATHS CD
+JOIN VACCINATIONS VC ON CD.LOCATION1 = VC.LOCATION2
+AND CD.DATE1 = VC.DATE2 WHERE CD.CONTINENT IS NOT NULL
 ORDER BY 2,3
